1af5e0cb2f49c2c047ef0360c5e1177b64127324
Dan Gebhardt <dan@cerebris.com>
2014-03-29 16:55:08 -0400

    introduce `fetchable`, `updateable` and `creatable`

--- lib/json/api/resource.rb
+++ lib/json/api/resource.rb
@@ -1,8 +1,17 @@
 module JSON
   module API
     class Resource
+      def fetchable(keys)
+        keys
+      end
 
+      def updateable(keys)
+        keys
+      end
 
+      def creatable(keys)
+        keys
+      end
     end
   end
 end
