7177778bf55b3979b2b9a71ee976bb77c3943388
Larry Gebhardt <larry@cerebris.com>
2014-07-10 15:52:51 -0400

    Add status

--- lib/json/api/error.rb
+++ lib/json/api/error.rb
@@ -9,6 +9,7 @@ module JSON
         @code           = options[:code]
         @path           = options[:path]
         @links          = options[:links]
+        @status         = options[:status]
       end
 
       def to_json
@@ -17,6 +18,7 @@ module JSON
         error_hash[:detail] = @detail unless @detail.nil?
         error_hash[:id] = @id unless @id.nil?
         error_hash[:href] = @href unless @href.nil?
+        error_hash[:status] = @status unless @status.nil?
         error_hash[:code] = @code unless @code.nil?
         error_hash[:path] = @path unless @path.nil?
         error_hash[:links] = @links unless @links.nil? || @links.blank?
