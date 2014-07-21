939132fd86763424a410bc62ffe2e5634d2ec5a3
Larry Gebhardt <larry@cerebris.com>
2014-07-21 13:11:21 -0400

    Removed unused to_json method

--- lib/json/api/error.rb
+++ lib/json/api/error.rb
@@ -14,19 +14,6 @@ module JSON
         @links          = options[:links]
         @status         = options[:status]
       end
-
-      def to_json
-        error_hash = {}
-        error_hash[:title] = @title unless @title.nil?
-        error_hash[:detail] = @detail unless @detail.nil?
-        error_hash[:id] = @id unless @id.nil?
-        error_hash[:href] = @href unless @href.nil?
-        error_hash[:status] = @status unless @status.nil?
-        error_hash[:code] = @code unless @code.nil?
-        error_hash[:path] = @path unless @path.nil?
-        error_hash[:links] = @links unless @links.nil? || @links.blank?
-        error_hash
-      end
     end
   end
 end
