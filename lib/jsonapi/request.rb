6dd3627551650b539ea00e687045a624e4296b09
Sol Irvine <sol@kbbpartners.com>
2015-03-29 13:18:22 +0900

    Check for type on updates should be unformatted string

--- lib/jsonapi/request.rb
+++ lib/jsonapi/request.rb
@@ -393,7 +393,7 @@ module JSONAPI
       end
 
       type = data[:type]
-      if type.nil? || type != @resource_klass._type.to_s
+      if type.nil? || unformat_key(type).to_s != @resource_klass._type.to_s
         raise JSONAPI::Exceptions::ParameterMissing.new(:type)
       end
 
