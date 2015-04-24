ec6511a69b11453fa635bfd3bd3197087904416b
Larry Gebhardt <larry@cerebris.com>
2015-04-24 10:57:46 -0400

    Add process_operation method that catches AR exceptions

--- lib/jsonapi/active_record_operations_processor.rb
+++ lib/jsonapi/active_record_operations_processor.rb
@@ -11,5 +11,16 @@ module JSONAPI
     def rollback
       raise ActiveRecord::Rollback
     end
+
+    def process_operation(operation)
+      operation.apply(@context)
+    rescue ActiveRecord::DeleteRestrictionError => e
+      record_locked_error = JSONAPI::Exceptions::RecordLocked.new(e.message)
+      return JSONAPI::OperationResult.new(record_locked_error.errors[0].code, nil, record_locked_error.errors)
+
+    rescue ActiveRecord::RecordNotFound
+      record_not_found = JSONAPI::Exceptions::RecordNotFound.new(operation.associated_key)
+      return JSONAPI::OperationResult.new(record_not_found.errors[0].code, nil, record_not_found.errors)
+    end
   end
 end
