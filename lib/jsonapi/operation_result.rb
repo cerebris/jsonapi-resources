8f00d42009d2290240ddafd833253b412de8a263
Larry Gebhardt <larry@cerebris.com>
2015-06-17 11:26:41 -0400

    Add options to OperationResults

--- lib/jsonapi/operation_result.rb
+++ lib/jsonapi/operation_result.rb
@@ -2,47 +2,51 @@ module JSONAPI
   class OperationResult
     attr_accessor :code
     attr_accessor :meta
+    attr_accessor :links
+    attr_accessor :options
 
-    def initialize(code)
+    def initialize(code, options = {})
       @code = code
-      @meta = {}
+      @options = options
+      @meta = options.fetch(:meta, {})
+      @links = options.fetch(:links, {})
     end
   end
 
   class ErrorsOperationResult < OperationResult
     attr_accessor :errors
 
-    def initialize(code, errors)
+    def initialize(code, errors, options = {})
       @errors = errors
-      super(code)
+      super(code, options)
     end
   end
 
   class ResourceOperationResult < OperationResult
     attr_accessor :resource
 
-    def initialize(code, resource)
+    def initialize(code, resource, options = {})
       @resource = resource
-      super(code)
+      super(code, options)
     end
   end
 
   class ResourcesOperationResult < OperationResult
     attr_accessor :resources
 
-    def initialize(code, resources)
+    def initialize(code, resources, options = {})
       @resources = resources
-      super(code)
+      super(code, options)
     end
   end
 
   class LinksObjectOperationResult < OperationResult
     attr_accessor :parent_resource, :association
 
-    def initialize(code, parent_resource, association)
+    def initialize(code, parent_resource, association, options = {})
       @parent_resource = parent_resource
       @association = association
-      super(code)
+      super(code, options)
     end
   end
 end
