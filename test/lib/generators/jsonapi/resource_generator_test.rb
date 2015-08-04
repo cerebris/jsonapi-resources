d2201289bb34ac7c3dda9cf761d3d24461e8adc1
Kevin Traver <kevintraver@gmail.com>
2015-08-03 23:15:14 -0600

    Cleanup destination root on teardown

--- test/lib/generators/jsonapi/resource_generator_test.rb
+++ test/lib/generators/jsonapi/resource_generator_test.rb
@@ -6,6 +6,11 @@ module Jsonapi
     tests ResourceGenerator
     destination Rails.root.join('../resources')
     setup :prepare_destination
+    teardown :cleanup_destination_root
+
+    def cleanup_destination_root
+      FileUtils.rm_rf destination_root
+    end
 
     test "resource is created" do
       run_generator ["post"]
