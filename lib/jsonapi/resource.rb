b945e1fea0dceddba32bf6d21b62c14c08135f05
Ben Armston <ben.armston@googlemail.com>
2015-12-02 16:35:09 +0000

    Validation errors can have metadata added to them

--- lib/jsonapi/resource.rb
+++ lib/jsonapi/resource.rb
@@ -121,6 +121,29 @@ module JSONAPI
       _model.errors.messages
     end
 
+    # Add metadata to validation error objects.
+    #
+    # Suppose `model_error_messages` returned the following error messages
+    # hash:
+    #
+    #   {password: ["too_short", "format"]}
+    #
+    # Then to add data to the validation error `validation_error_metadata`
+    # could return:
+    #
+    #   {
+    #     password: {
+    #       "too_short": {"minimum_length" => 6},
+    #       "format": {"requirement" => "must contain letters and numbers"}
+    #     }
+    #   }
+    #
+    # The specified metadata is then be merged into the validation error
+    # object.
+    def validation_error_metadata
+      {}
+    end
+
     # Override this to return resource level meta data
     # must return a hash, and if the hash is empty the meta section will not be serialized with the resource
     # meta keys will be not be formatted with the key formatter for the serializer by default. They can however use the
