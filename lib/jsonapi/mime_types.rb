2753291576c1a2841b57063965010688630655b3
Jesper Josefsson <jesper.josefsson@gmail.com>
2016-02-29 12:16:45 +0100

    Rails 5 beta 3-compatible parameter parsing

--- lib/jsonapi/mime_types.rb
+++ lib/jsonapi/mime_types.rb
@@ -1,15 +1,28 @@
 module JSONAPI
   MEDIA_TYPE = 'application/vnd.api+json'
-end
 
-Mime::Type.register JSONAPI::MEDIA_TYPE, :api_json
+  module MimeTypes
+    def self.install
+      Mime::Type.register JSONAPI::MEDIA_TYPE, :api_json
 
-parsers = Rails::VERSION::MAJOR >= 5 ?
-            ActionDispatch::Http::Parameters :
-            ActionDispatch::ParamsParser
+      if Rails::VERSION::MAJOR >= 5
+        parsers = ActionDispatch::Request.parameter_parsers.merge(
+          Mime::Type.lookup(JSONAPI::MEDIA_TYPE).symbol => parser
+        )
+        ActionDispatch::Request.parameter_parsers = parsers
+      else
+        ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup(JSONAPI::MEDIA_TYPE)] = parser
+      end
+    end
 
-parsers::DEFAULT_PARSERS[Mime::Type.lookup(JSONAPI::MEDIA_TYPE)] = lambda do |body|
-  data = JSON.parse(body)
-  data = {:_json => data} unless data.is_a?(Hash)
-  data.with_indifferent_access
+    def self.parser
+      lambda do |body|
+        data = JSON.parse(body)
+        data = {:_json => data} unless data.is_a?(Hash)
+        data.with_indifferent_access
+      end
+    end
+  end
+
+  MimeTypes.install
 end
