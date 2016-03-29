120f06e1f2b8e4269ff6b92006c1d30ace004eb5
David Stosik <davidstosik@users.noreply.github.com>
2016-03-29 18:29:28 +0900

    Allow custom constraints on `jsonapi_resources` routes.

--- lib/jsonapi/routing_ext.rb
+++ lib/jsonapi/routing_ext.rb
@@ -69,7 +69,8 @@ module ActionDispatch
           options[:path] = format_route(@resource_type)
 
           if res.resource_key_type == :uuid
-            options[:constraints] = {id: /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/}
+            options[:constraints] ||= {}
+            options[:constraints][:id] ||= /[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/
           end
 
           if options[:except]
