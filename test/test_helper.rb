21b865b63e812d9891b57958d9b79a0f4a441362
Larry Gebhardt <larry@cerebris.com>
2016-04-15 09:41:09 -0400

    Update testing notes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,11 +2,16 @@ require 'simplecov'
 
 # To run tests with coverage:
 # COVERAGE=true bundle exec rake test
+
+# To test on a specific rails version use this:
+# export RAILS_VERSION=4.2.6; bundle update rails; bundle exec rake test
+# export RAILS_VERSION=5.0.0.beta3; bundle update rails; bundle exec rake test
+
+# We are no longer having Travis test Rails 4.0.x., but you can try it with:
+# export RAILS_VERSION=4.0.0; bundle update rails; bundle exec rake test
+
 # To Switch rails versions and run a particular test order:
-# export RAILS_VERSION=4.2.0; bundle update rails; bundle exec rake TESTOPTS="--seed=39333" test
-# We are no longer having Travis test Rails 4.0.x. To test on Rails 4.0.x use this:
-# export RAILS_VERSION=4.2.5; bundle update rails; bundle exec rake test
-# export RAILS_VERSION=5.0.0.beta1.1; bundle update rails; bundle exec rake test
+# export RAILS_VERSION=4.2.6; bundle update rails; bundle exec rake TESTOPTS="--seed=39333" test
 
 if ENV['COVERAGE']
   SimpleCov.start do
