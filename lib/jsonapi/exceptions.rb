60bdd2eca97d692718a921f78d243e481cc0d67d
Arne Zeising <arne.zeising@gmail.com>
2018-07-05 16:28:11 +0200

    Remove now superfluous error

--- lib/jsonapi/exceptions.rb
+++ lib/jsonapi/exceptions.rb
@@ -141,26 +141,6 @@ module JSONAPI
       end
     end
 
-
-    class HasManyRelationExists < Error
-      attr_accessor :id
-
-      def initialize(id, error_object_overrides = {})
-        @id = id
-        super(error_object_overrides)
-      end
-
-      def errors
-        [create_error_object(code: JSONAPI::RELATION_EXISTS,
-                             status: :bad_request,
-                             title: I18n.translate('jsonapi-resources.exceptions.has_many_relation.title',
-                                                   default: 'Relation exists'),
-                             detail: I18n.translate('jsonapi-resources.exceptions.has_many_relation.detail',
-                                                    default: "The relation to #{id} already exists.",
-                                                    id: id))]
-      end
-    end
-
     class BadRequest < Error
       def initialize(exception, error_object_overrides = {})
         @exception = exception
