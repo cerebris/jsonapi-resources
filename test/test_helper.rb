248412eaea30a7ddf66ec8d47bb971a2ca6d4a51
Larry Gebhardt <larry@cerebris.com>
2021-04-07 16:21:22 -0400

    Refine when config.active_record.sqlite3.represent_boolean_as_integer is set
    Tests with rails > 6.1.1 breaks with this option

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -62,7 +62,7 @@ class TestApp < Rails::Application
   config.active_support.halt_callback_chains_on_return_false = false
   config.active_record.time_zone_aware_types = [:time, :datetime]
   config.active_record.belongs_to_required_by_default = false
-  unless Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR < 2
+  unless Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR < 2 || Rails::VERSION::MAJOR == 6 && Rails::VERSION::MINOR >= 1
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
