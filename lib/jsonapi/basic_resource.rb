695bb7cb51cec2fb54dfb8032d0e07ea1d577c51
Larry Gebhardt <larry@cerebris.com>
2021-04-12 15:00:40 -0400

    Fix some testing issues including flappy tests, simplify quoting generated SQL strings (#1363)
    * Refine when config.active_record.sqlite3.represent_boolean_as_integer is set
    
    Tests with rails > 6.1.1 breaks with this option
    
    * Update rails 6.1 tested version
    
    * Add quoting for fields in built up sql
    
    * Remove tests for generated sql statements
    
    These are changing with new rails versions and it's creating a lot of false failures
    
    * Remove tests for generated sql statements using joins_left
    
    * Fix default sort for when empty array is provided
    
    Fixes issue with relationship requests not getting a default sort
    
    * Add helper methods for generating sql fields with aliases and quotes

--- lib/jsonapi/basic_resource.rb
+++ lib/jsonapi/basic_resource.rb
@@ -1057,7 +1057,7 @@ module JSONAPI
       end
 
       def construct_order_options(sort_params)
-        sort_params ||= default_sort
+        sort_params = default_sort if sort_params.blank?
 
         return {} unless sort_params
 
