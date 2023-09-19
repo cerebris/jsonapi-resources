9837c367aedf058793633df709742a704d0e24e2
lgebhardt <larry@cerebris.com>
2023-09-19 14:31:32 -0400

    Add frozen_string_literal: true

--- lib/jsonapi/active_relation/adapters/join_left_active_record_adapter.rb
+++ lib/jsonapi/active_relation/adapters/join_left_active_record_adapter.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module JSONAPI
   module ActiveRelation
     module Adapters
--- lib/jsonapi/simple_resource.rb
+++ lib/jsonapi/simple_resource.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'jsonapi/callbacks'
 require 'jsonapi/configuration'
 
