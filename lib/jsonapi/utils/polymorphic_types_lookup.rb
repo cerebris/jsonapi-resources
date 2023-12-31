e67d6764e4f04e165a8b20214869f6684581bace
Benjamin Fleischer <github@benjaminfleischer.com>
2023-12-31 17:14:23 +0530

    fix: more flexible polymorphic types lookup

--- lib/jsonapi/utils/polymorphic_types_lookup.rb
+++ lib/jsonapi/utils/polymorphic_types_lookup.rb
@@ -6,7 +6,16 @@ module JSONAPI
       extend self
 
       def polymorphic_types(name)
-        polymorphic_types_lookup[name.to_sym]
+        polymorphic_types_lookup.fetch(name.to_sym) do
+          klass = name.classify.safe_constantize
+          if klass.nil?
+            warn "[POLYMORPHIC TYPE NOT FOUND] No polymorphic types found for #{name}"
+          else
+            polymorphic_type = format_polymorphic_klass_type(klass)
+            warn "[POLYMORPHIC TYPE] Found polymorphic types through reflection for #{name}: #{polymorphic_type}"
+            polymorphic_types_lookup[name.to_sym] = [polymorphic_type]
+          end
+        end
       end
 
       def polymorphic_types_lookup
@@ -17,13 +26,27 @@ module JSONAPI
         {}.tap do |hash|
           ObjectSpace.each_object do |klass|
             next unless Module === klass
-            if ActiveRecord::Base > klass
+            is_active_record_inspectable = ActiveRecord::Base > klass
+            is_active_record_inspectable &&= klass.respond_to?(:reflect_on_all_associations, true)
+            is_active_record_inspectable &&= format_polymorphic_klass_type(klass).present?
+            if is_active_record_inspectable
               klass.reflect_on_all_associations(:has_many).select { |r| r.options[:as] }.each do |reflection|
-                (hash[reflection.options[:as]] ||= []) << klass.name.underscore
+                (hash[reflection.options[:as]] ||= []) << format_polymorphic_klass_type(klass).underscore
               end
             end
           end
         end
+      end
+
+      def format_polymorphic_klass_type(klass)
+        klass.name ||
+          begin
+            klass.model_name.name
+          rescue ArgumentError => ex
+            # klass.base_class may be nil
+            warn "[POLYMORPHIC TYPE] #{__callee__} #{klass} #{ex.inspect}"
+            nil
+          end
       end
     end
   end
