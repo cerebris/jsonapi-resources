7946f835e3548a011276aa4f026c5334192414f9
Benjamin Fleischer <github@benjaminfleischer.com>
2024-01-24 09:10:08 -0600

    test: polymorphic type lookup

--- lib/jsonapi/utils/polymorphic_types_lookup.rb
+++ lib/jsonapi/utils/polymorphic_types_lookup.rb
@@ -6,15 +6,13 @@ module JSONAPI
       extend self
 
       def polymorphic_types(name, rebuild: false)
-        polymorphic_types_lookup(rebuild: rebuild).fetch(name.to_sym) do
-          klass = name.classify.safe_constantize
-          if klass.nil?
-            warn "[POLYMORPHIC TYPE NOT FOUND] No polymorphic types found for #{name}"
-          else
-            polymorphic_type = format_polymorphic_klass_type(klass)
-            warn "[POLYMORPHIC TYPE] Found polymorphic types through reflection for #{name}: #{polymorphic_type}"
-            polymorphic_types_lookup[name.to_sym] = [polymorphic_type]
-          end
+        polymorphic_types_lookup(rebuild: rebuild).fetch(name.to_sym, &handle_polymorphic_type_name_found)
+      end
+
+      def handle_polymorphic_type_name_found
+        @handle_polymorphic_type_name_found ||= lambda do |name|
+          warn "[POLYMORPHIC TYPE NOT FOUND] No polymorphic types found for #{name}"
+          nil
         end
       end
 
@@ -51,31 +49,34 @@ module JSONAPI
         {}.tap do |lookup|
           ObjectSpace.each_object do |klass|
             next unless Module === klass
-            is_active_record_inspectable = ActiveRecord::Base > klass
-            is_active_record_inspectable &&= klass.respond_to?(:reflect_on_all_associations, true)
-            is_active_record_inspectable &&= format_polymorphic_klass_type(klass).present?
-            if is_active_record_inspectable
-              add_polymorphic_types_lookup(klass: klass, lookup: lookup)
-            end
+            next unless ActiveRecord::Base > klass
+            add_polymorphic_types_lookup(klass: klass, lookup: lookup)
           end
         end
       end
 
+      # TODO(BF): Consider adding the following conditions
+      # is_active_record_inspectable = true
+      # is_active_record_inspectable &&= klass.respond_to?(:reflect_on_all_associations, true)
+      # is_active_record_inspectable &&= format_polymorphic_klass_type(klass).present?
+      # return unless is_active_record_inspectable
       def add_polymorphic_types_lookup(klass:, lookup:)
         klass.reflect_on_all_associations(:has_many).select { |r| r.options[:as] }.each do |reflection|
           (lookup[reflection.options[:as]] ||= []) << format_polymorphic_klass_type(klass).underscore
         end
       end
 
+      # TODO(BF): Consider adding the following conditions
+      # klass.name ||
+      #   begin
+      #     klass.model_name.name
+      #   rescue ArgumentError => ex
+      #     # klass.base_class may be nil
+      #     warn "[POLYMORPHIC TYPE] #{__callee__} #{klass} #{ex.inspect}"
+      #     nil
+      #   end
       def format_polymorphic_klass_type(klass)
-        klass.name ||
-          begin
-            klass.model_name.name
-          rescue ArgumentError => ex
-            # klass.base_class may be nil
-            warn "[POLYMORPHIC TYPE] #{__callee__} #{klass} #{ex.inspect}"
-            nil
-          end
+        klass.name
       end
     end
   end
